e6055c374d1a2754bfaa4182c8a0dbccbbb17ccb
mwcole <mwcole@mwcole.net>
2019-04-23 17:12:55 -0400

    Init update

--- __init__.py
+++ __init__.py
@@ -5,3 +5,12 @@
 #    __all__.append(module_name)
 #    _module = loader.find_module(module_name).load_module(module_name)
 #    globals()[module_name] = _module
+
+from . import actflowcomp
+from . import connectivity_estimation
+from . import dependencies
+from . import infotransfermapping
+from . import pipelines
+from . import preprocessing
+from . import simulations
+from . import tools
