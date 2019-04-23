e5cd7eda2a9d97c6341fbbcd288a5a0c2aec3ef9
mwcole <mwcole@mwcole.net>
2019-04-23 17:10:06 -0400

    update

--- taku_repo/demo.py
+++ taku_repo/demo.py
@@ -1,11 +1,12 @@
 # Taku Ito
 # 4/11/2019
 # Demo to implement multiple regression FC on glasser parcels after removing neighboring vertices (10mm dilation) from fc estimates
+
 import numpy as np
 import h5py
-import multregfc_glasser_parcellation as fccalc 
+from . import multregfc_glasser_parcellation as fccalc
 import importlib
-fccalc = importlib.reload(fccalc)
+#fccalc = importlib.reload(fccalc)
 
 data = h5py.File('100206_glmOutput_64k_data.h5','r')
 
