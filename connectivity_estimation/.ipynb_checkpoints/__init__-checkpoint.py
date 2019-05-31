7d0e93ae994201c600195b58dd1487d4ae84ab7b
ljhearne <ljh118@rutgers.edu>
2019-05-31 11:00:46 -0400

    fixed pca reg code

--- connectivity_estimation/.ipynb_checkpoints/__init__-checkpoint.py
+++ connectivity_estimation/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,4 +1,5 @@
 from .multregconn import *
+from .pc_multregconn import *
 from .calcconn_parcelwise_noncircular_surface import *
 from .calcactivity_parcelwise_noncircular_surface import *
 from .corrcoefconn import *
