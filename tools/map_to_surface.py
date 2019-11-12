Fixed bug in max_t calculation, added feature to flip hemispheres for map_to_surface

tools/map_to_surface.py
@@ -9,7 +9,7 @@ import pkg_resources
 dependenciesdir = pkg_resources.resource_filename('ActflowToolbox.dependencies', '/')
 glasserfile2=dependenciesdir+'ColeAnticevicNetPartition/CortexSubcortex_ColeAnticevic_NetPartition_wSubcorGSR_parcels_LR.dlabel.nii'
 
-def map_to_surface(mat,filename,nParcels=360,glasserfile2=glasserfile2):
+def map_to_surface(mat,filename,nParcels=360,glasserfile2=glasserfile2,fliphemispheres=False):
     """
     Maps a region X column 2d matrix into a dscalar file with 64k vertices
     Uses the Glasser et al. 2016 ROI parcellation
@@ -19,8 +19,19 @@ def map_to_surface(mat,filename,nParcels=360,glasserfile2=glasserfile2):
                     mat can either be 360 mat or ~59k mat. If 360, will automatically map back to ~59k
 
         filename:   a string indicating the directory + filename of the output. Do not include a suffix (e.g., ".dscalar.nii" to the file. Suffixes will be added automatically.
+        
+        fliphemispheres: If the data were originally loaded using RL (right hemisphere then left) convention the data should be
+        flipped, since CAB-NP uses LR (left hemisphere then right). A setting of True will flip the hemispheres.
 
     """
+    
+    if fliphemispheres:
+        print('Flipping hemispheres')
+        newmat=np.zeros(mat.shape)
+        newmat[0:180]=mat[180:360]
+        newmat[180:360]=mat[0:180]
+        mat=newmat.copy() 
+    
     #### Load glasser atlas
     glasser2 = nib.load(glasserfile2).get_data()
     glasser2 = np.squeeze(glasser2)