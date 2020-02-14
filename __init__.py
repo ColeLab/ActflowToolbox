7c25601d30432361767770042d920be4767af36e
Luke Hearne <ljh118@rutgers.edu>
2020-02-14 11:59:00 -0500

    commented out removed dirs

--- __init__.py
+++ __init__.py
@@ -4,9 +4,9 @@ from .actflowcomp import *
 from . import model_compare
 from . import connectivity_estimation
 from . import dependencies
-from . import infotransfermapping
-from . import pipelines
-from . import preprocessing
+#from . import infotransfermapping
+#from . import pipelines
+#from . import preprocessing
 from . import simulations
 from . import tools
 #from . import latent_connectivity
