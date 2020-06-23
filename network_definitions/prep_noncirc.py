3386497e7ec20a7a75ff4d308fbbcc58fbba4f0d
mwcole <mwcole@mwcole.net>
2020-06-23 14:06:25 -0400

    Make prep_noncirc ready to run from scratch

--- network_definitions/prep_noncirc.py
+++ network_definitions/prep_noncirc.py
@@ -11,10 +11,10 @@ from calc_removal_coords import *
 
 #Second, run calc_removal_coords.py to calculate which grayordinates to remove for each target region
 #This second step speeds up the non-circular code by preventing the need to load all of the dilated masks one-at-a-time
-#calc_removal_coords(cortex_only=True, verbose=True)
-#calc_removal_coords(cortex_only=False, verbose=True)
+calc_removal_coords(cortex_only=True, verbose=True)
+calc_removal_coords(cortex_only=False, verbose=True)
 
 #Alternate: remove all parcels with any parts within X mm of target parcel
-#calc_removal_coords(cortex_only=True, verbose=True, parcel_level=True)
+calc_removal_coords(cortex_only=True, verbose=True, parcel_level=True)
 calc_removal_coords(cortex_only=False, verbose=True, parcel_level=True)
 
