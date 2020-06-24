eba453e85353ae58c271fbd2130b63faba2c2fdf
mwcole <mwcole@mwcole.net>
2020-06-24 10:43:52 -0400

    Fixed bug relevant to cortex+subcortex non-circular analysis (listing parcels to remove by index rather than value)

--- network_definitions/.ipynb_checkpoints/prep_noncirc-checkpoint.py
+++ network_definitions/.ipynb_checkpoints/prep_noncirc-checkpoint.py
@@ -9,12 +9,12 @@ from calc_removal_coords import *
 #First run dilateParcels.py, to dilate all of the parcels in the CIFTI network partition
 #dilateParcels()
 
-# #Second, run calc_removal_coords.py to calculate which grayordinates to remove for each target region
-# #This second step speeds up the non-circular code by preventing the need to load all of the dilated masks one-at-a-time
-# calc_removal_coords(cortex_only=True, verbose=True)
-# calc_removal_coords(cortex_only=False, verbose=True)
+#Second, run calc_removal_coords.py to calculate which grayordinates to remove for each target region
+#This second step speeds up the non-circular code by preventing the need to load all of the dilated masks one-at-a-time
+calc_removal_coords(cortex_only=True, verbose=True)
+calc_removal_coords(cortex_only=False, verbose=True)
 
 #Alternate: remove all parcels with any parts within X mm of target parcel
-#calc_removal_coords(cortex_only=True, verbose=True, parcel_level=True)
+calc_removal_coords(cortex_only=True, verbose=True, parcel_level=True)
 calc_removal_coords(cortex_only=False, verbose=True, parcel_level=True)
 
